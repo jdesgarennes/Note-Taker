2fd53c84cbff56bee399c20a7f771acdbabb7009
jdesgarennes <johndesgarennes@gmail.com>
2021-10-29 17:14:15 -0700

    Added post functionality

--- Develop/start.js
+++ Develop/start.js
@@ -1,15 +1,17 @@
 
-
+const fs = require('fs');
 const express = require('express');
 const path = require('path');
+const { JSHandle } = require('puppeteer');
 const app = express();
-const db = require('./db/db.json');
 
 // defining port and console log the output.
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, ()=> console.log(`Server started on port: ${PORT}`));
 
 //sets static folder to public
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, './public')))
 
 // sets route for /notes 
@@ -19,7 +21,30 @@ app.get('/notes', (req,res) =>{
 
 
 // sets API route for reading db.json
-app.get('/api/notes', (req, res) => res.json(db));
 
-// Need to create API PUT
+app.get('/api/notes', (req, res) => {
+
+    fs.readFile('./db/db.json', 'utf-8',(err, data) => {
+        if (err) throw err;
+        res.json(JSON.parse(data));
+      });
+
+});
+// Sets API post for saving to DB
+app.post('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf-8',(err, data) => {
+        if (err) throw err;
+        let newdata = JSON.parse(data);
+        newdata.push(req.body);
+        fs.writeFile('./db/db.json',JSON.stringify(newdata), (err)=>
+        {
+            if (err) throw err;
+            console.log("xyz");  
+             res.json(newdata);
+        }
+
+
+    )});
+    })
+
 
